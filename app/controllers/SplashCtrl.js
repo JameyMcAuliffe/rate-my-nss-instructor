4e6b4c80bf90fdf56280c5a86787cd61c140da51
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2016-08-01 23:10:14 -0500

    login/registration/nav partials with basic functionality added

--- app/controllers/SplashCtrl.js
+++ app/controllers/SplashCtrl.js
@@ -1,5 +1,22 @@
 "use strict";
 
+//Students button bringing up login modal
 app.controller("SplashCtrl", function($scope) {
+	$scope.studentsButtonClick = function() {
 
+		$scope.login_register = true;		
+		console.log("studentsButtonClick");
+	};
+
+	$scope.activateLogin = function() {
+		$scope.loginMode = true;
+		$scope.registerMode = false;
+		console.log("activateLogin");
+	};
+
+	$scope.activateRegister = function() {
+		$scope.loginMode = false;
+		$scope.registerMode = true;
+		console.log("activateRegister");
+	};
 });
