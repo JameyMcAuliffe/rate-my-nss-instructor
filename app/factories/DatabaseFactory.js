c57fc4a16c6eb3bea6b640358bb3360eebccf7c4
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2016-08-04 11:47:10 -0500

    full rating functionality, ratings objects are stored to firebase

--- app/factories/DatabaseFactory.js
+++ app/factories/DatabaseFactory.js
@@ -2,5 +2,20 @@
 
 app.factory("DatabaseFactory", function(FirebaseURL, $q, $http, AuthFactory) {
 
+	let postNewRating = function(newRating) {
+		return $q(function(resolve, reject) {
+			$http.post(
+				`${FirebaseURL}/users.json`,
+				JSON.stringify(newRating)
+			)
+			.success(function(ObjFromFirebase) {
+				resolve(ObjFromFirebase);
+			})
+			.error(function(error) {
+				reject(error);
+			});
+		});
+	};
 
-})
+	return {postNewRating};
+});
