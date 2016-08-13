afdcb71dd5bc3dada187656e53d3df5f14e09424
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2016-08-13 14:40:26 -0400

    added toasts

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -5,5 +5,6 @@ app.controller("NavCtrl", function($scope, AuthFactory) {
 	//ng-click function for logout link
 	$scope.logout = function() {
 		AuthFactory.logout();
+		Materialize.toast('Good Day To You', 3000, 'rounded');
 	};
 });
