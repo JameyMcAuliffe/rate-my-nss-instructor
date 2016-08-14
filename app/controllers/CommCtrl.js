cfd44237ed9c10ab91cdb7bae6d21a17a80582e7
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2016-08-14 17:51:00 -0500

    message board skeleton

--- app/controllers/CommCtrl.js
+++ app/controllers/CommCtrl.js
@@ -3,9 +3,9 @@
 app.controller("CommCtrl", function($scope, DatabaseFactory, $location) {
 
 	//creates an array of event objects which are used to populate the dom
-	DatabaseFactory.getEvents()
-	.then(function(eventsArray) {
-		$scope.events = eventsArray;
-		console.log("eventsArray", $scope.events);
+	DatabaseFactory.getThreads()
+	.then(function(threadsArray) {
+		$scope.threads = threadsArray;
+		console.log("threadsArray", $scope.threads);
 	});
 });
